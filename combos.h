5a4990f398dd9a714fc60c9c6c5e423e22067682
Turukano <kennel@posteo.de>
2023-05-11 17:25:25 +0200

    1.4

--- combos.h
+++ combos.h
@@ -1,1 +1,22 @@
 //COmbo mit NAVNUM und STRG für modded layer
+
+enum combos {
+  AB_ESC,
+  JK_TAB,
+  QW_SFT,
+  SD_LAYER,
+  COMBO_LENGTH
+};
+uint16_t COMBO_LEN = COMBO_LENGTH; // remove the COMBO_COUNT define and use this instead!
+
+const uint16_t PROGMEM ab_combo[] = {KC_A, KC_B, COMBO_END};
+const uint16_t PROGMEM jk_combo[] = {KC_J, KC_K, COMBO_END};
+const uint16_t PROGMEM qw_combo[] = {KC_Q, KC_W, COMBO_END};
+const uint16_t PROGMEM sd_combo[] = {KC_S, KC_D, COMBO_END};
+
+combo_t key_combos[] = {
+  [AB_ESC] = COMBO(ab_combo, KC_ESC),
+  [JK_TAB] = COMBO(jk_combo, KC_TAB),
+  [QW_SFT] = COMBO(qw_combo, KC_LSFT),
+  [SD_LAYER] = COMBO(sd_combo, MO(_LAYER)),
+};
--- keymap.c
+++ keymap.c
@@ -28,7 +28,6 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 };
 
 #include "tapdances.h"
-#include "hungarian_dances.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [DEF] = LAYOUT(
